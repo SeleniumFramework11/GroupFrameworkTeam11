66ae0233b785f305fd8905fd142e65df763b5e9c
OrfattChowdhury1 <orfatt11@gmail.com>
2019-01-05 15:47:39 -0800

    Group Framework

---
+++ Generic/src/main/java/base/CommonAPI.java
@@ -0,0 +1,160 @@
+package base;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static jdk.nashorn.internal.objects.NativeDate.getTime;
+
+public class CommonAPI {
+    public static WebDriver driver = null;
+    public static Actions builder = null;
+    public static WebDriverWait wait;
+
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\orfat\\eclipse-workspace\\seleniumindividualframework\\generic\\browser-driver\\chromedriver.exe");
+        //setUpBrowserStack();
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        builder = new Actions(driver);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.foxnews.com/");
+        driver.manage().window().fullscreen();
+
+
+
+    }
+    public static ExtentReports extent;
+    @BeforeSuite
+    public void extentSetup(ITestContext context) { extent = ExtentManager.getInstance();}
+    @BeforeMethod
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        String methodName = method.getName().toLowerCase();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(className);
+    }
+    protected String getStackTrace (Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getStartMillis()));
+
+        for (String group: result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passes");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+        ExtentTestManager.endTest();
+        extent.flush();
+        if (result.getStatus() == ITestResult.FAILURE) {
+            captureScreenshot(driver, result.getName());
+        }
+        driver.quit();
+
+    }
+    public void captureScreenshot(WebDriver driver, String screenshotName) {
+        DateFormat df = new SimpleDateFormat("MM.dd.yyyy-HH:mma");
+        Date date = new Date();
+        df.format(date);
+
+
+
+
+    }
+
+    @AfterSuite
+    public void generateReport() { extent.close();}
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+    public void waitToBeVisible(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    public void waitToBeVisible(String xpathLocator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathLocator)));
+    }
+    public void setUpBrowserStack() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("browser","chrome");
+        cap.setCapability("browser_version","68.0");
+        cap.setCapability("os", "OS X"); //put your operating system
+        cap.setCapability("os_version", "Sierra"); //put your version
+        String browserStackUrl = "https://afiafarjana1:9Z5U2U9zmF6Uq6QUr9pi@hub-cloud.browserstack.com/wd/hub";
+        URL serverUrl = new URL(browserStackUrl);
+        driver = new RemoteWebDriver(serverUrl,cap);
+    }
+    public static String convertToString(String st){
+        String splitString ;
+        splitString = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(st), ' ');
+        return splitString;
+    }
+    public void inputValueInTextBoxByWebElement(WebElement webElement, String value){
+        webElement.sendKeys(value + Keys.ENTER);
+    }
+    public void typeOnInputField(String locator, String value) {
+        driver.findElement(By.xpath(locator)).sendKeys(value, Keys.ENTER);
+    }
+    public void clearInputBox(WebElement webElement){
+        webElement.clear();
+    }
+    public static void sleepFor(int sec)throws InterruptedException {
+        Thread.sleep(sec * 1000);
+    }
+    public List<String> getTextFromWebElements(WebElement element){
+        List<WebElement> elements = new ArrayList<WebElement>();
+        List<String> text = new ArrayList<String>();
+        for(WebElement e:elements){
+            text.add(e.getText());
+        }
+        return text;
+    }
+
+
+
+
+
+
+
+
+
+}
