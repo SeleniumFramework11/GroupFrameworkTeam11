980462696f3198ccab48d0d391069267a3b77381
imohammed <iliasmohammed0@gmail.com>
2019-01-06 15:14:42 -0500

    Branch

--- Generic/src/main/java/reporting/TestLogger.java
+++ Generic/src/main/java/reporting/TestLogger.java
@@ -1,7 +1,8 @@
 package reporting;
 
 import com.relevantcodes.extentreports.LogStatus;
-import org.apache.commons.lang3.StringUtils;
+
+import org.apache.commons.codec.binary.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.testng.Reporter;
 
